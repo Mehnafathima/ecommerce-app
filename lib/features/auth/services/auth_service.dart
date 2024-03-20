9edc347af50690c3916a83df1e826e87e829c6ef
Mehna <mehnafathima777@gmail.com>
2024-03-20 23:28:18 +0530

    20324commit

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,20 +1,20 @@
 // import 'dart:convert';
 
-// import 'package:ecommerce_app/common/widgets/bottom_bar.dart';
-// import 'package:ecommerce_app/constants/error_handling.dart';
-// import 'package:ecommerce_app/constants/global_variables.dart';
-// import 'package:ecommerce_app/constants/utils.dart';
-// import 'package:ecommerce_app/models/user.dart';
-// import 'package:ecommerce_app/providers/user_provider.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:provider/provider.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+// // import 'package:ecommerce_app/common/widgets/bottom_bar.dart';
+// // import 'package:ecommerce_app/constants/error_handling.dart';
+// // import 'package:ecommerce_app/constants/global_variables.dart';
+// // import 'package:ecommerce_app/constants/utils.dart';
+// // import 'package:ecommerce_app/servers/models/user.dart';
+// // import 'package:ecommerce_app/providers/user_provider.dart';
+// // import 'package:flutter/material.dart';
+// // import 'package:http/http.dart' as http;
+// // import 'package:provider/provider.dart';
+// // import 'package:shared_preferences/shared_preferences.dart';
 
 // class AuthService {
 //   // sign up user
 //   void signUpUser({
-//     required BuildContext context,
+//     // required BuildContext context,
 //     required String email,
 //     required String password,
 //     required String name,
